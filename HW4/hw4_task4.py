955a5d25c46e1adf564298bf1fb82892d40f9cee
tomho3203 <tom.q.ho-1@ou.edu>
2024-10-20 19:44:00 -0500

    Complete task 4

--- HW4/hw4_task4.py
+++ HW4/hw4_task4.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 # import necessary library 
 # if you need more libraries, just import them
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 # ......
 # --- end of task --- #
 
@@ -14,7 +15,7 @@ from sklearn.linear_model import LogisticRegression
 # in array "data", each row represents a patient 
 # each column represents an attribute of patients 
 # last column is the binary label: 1 means the patient has diabetes, 0 means otherwise
-data = np.loadtxt('diabetes.csv', delimiter=',', skiprows=1)
+data = np.loadtxt('HW4/diabetes.csv', delimiter=',', skiprows=1)
 [n,p] = np.shape(data)
 
 # always use last 25% data for testing 
@@ -28,7 +29,7 @@ label_test = data[n-num_test:,-1]
 # pick 8 values for array "num_train_per" e.g., 0.5 means using 50% of the available data for training 
 # You should aim to observe overiftting (and normal performance) from these 8 values 
 # Note: maximum percentage is 0.75
-num_train_per = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+num_train_per = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75]
 # --- end of task --- #
 
 er_train_per = []
@@ -45,27 +46,30 @@ for per in num_train_per:
     
     # --- Your Task --- #
     # now, training your model using training data 
-    # (sample_train, label_train)
-    # ......
-    # ......
+    model.fit(sample_train, label_train)
 
     # now, evaluate training error (not MSE) of your model 
     # store it in "er_train"
-    # ......
+    train_predictions = model.predict(sample_train)
+    er_train = 1 - accuracy_score(label_train, train_predictions)
     er_train_per.append(er_train)
     
     # now, evaluate testing error (not MSE) of your model 
     # store it in "er_test"
-    # ......
+    test_predictions = model.predict(sample_test)
+    er_test = 1 - accuracy_score(label_test, test_predictions)
     er_test_per.append(er_test)
     # --- end of task --- #
     
-plt.figure()    
-plt.plot(num_train_per,er_train_per, label='Training Error')
-plt.plot(num_train_per,er_test_per, label='Testing Error')
+plt.figure(figsize=(8, 6))
+plt.plot(num_train_per, er_train_per, label='Training Error', color='blue', marker='o')
+plt.plot(num_train_per, er_test_per, label='Testing Error', color='red', marker='o')
 plt.xlabel('Percentage of Training Data')
 plt.ylabel('Classification Error')
+plt.title('Impact of Training Data Size on Classification Performance')
 plt.legend()
+plt.grid(True)
+plt.show()
 
 
 
